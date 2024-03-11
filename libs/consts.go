00a5af22bf2d6846f3610e39195b6f9b646c5d11
wenshenghong <wenshenghong@intern.douban.com>
2024-03-11 13:10:09 +0800

    rebase

--- libs/consts.go
+++ libs/consts.go
@@ -4,8 +4,14 @@ package libs
 type User struct {
 	UserID    string `json:"userid"`
 	Password  string `json:"password"`
+	Cookies   []byte `json:"cookies,omitempty"`
 	Latitude  string `json:"latitude,omitempty"`  // 纬度
 	Longitude string `json:"longitude,omitempty"` // 经度
 	IsVIP     bool   `json:"isvip,omitempty"`     // 是否是VIP用户
-	GroupID   int    `json:"groupid,omitempty"`
+	GroupID   int    `json:"groupid,omitempty"`   // 小组
+}
+
+type Group struct {
+	ID    int
+	Users []User
 }
